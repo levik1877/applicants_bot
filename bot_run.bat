021df32c3038cc37603e3aebb29b0a735a83db6b
FARAON <levvolchenko@bk.ru>
2023-11-14 16:17:08 +0300

    Made some changes

--- bot_run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call %~dp0application_bot\venv\Scripts\activate
-
-cd %~dp0application_bot
-
-python main.py
-
-pause
--- main.py
+++
@@ -1,4 +0,0 @@
-import telegram_DB.users_data as db
-from aiogram import Bot, types, Dispatcher
-
-
